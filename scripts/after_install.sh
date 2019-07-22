48b41e7992fd224d99dd1fb39cc745d04046e2f7
Erhan Asikoglu <easikoglu@gmail.com>
2019-07-22 16:10:31 +0300

    bitbucket pipelines updated

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,10 +1,7 @@
 #!/usr/bin/env bash
 source /home/ec2-user/.bash_profile
-cd /home/ec2-user/mome-front-end/
+cd /home/ec2-user/my-app/
 npm install
-npm install -g grunt-cli
-npm install grunt --save-dev
-grunt env-test
 
 
 
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source /home/ec2-user/.bash_profile
-cd /home/ec2-user/mome-front-end/
+cd /home/ec2-user/my-app/
 pm2 kill
 pm2 start process.config.json --env test
